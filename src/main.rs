4afe2afc29ea004ca7f0c0c3ac7fe32627ddb5c7
Clement-Devevey <clement.devevey@lilo.org>
2023-07-03 18:34:34 +0200

    alternative mode allowing sending outputs to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 pub mod genchars;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use itertools::Itertools;
 use std::fs::File;
+use std::io;
 use std::io::Write;
 use std::mem;
 use std::time::{Instant, Duration};
@@ -24,6 +25,17 @@ struct Cli {
     /// Delay in ms to wait between every permutation
     #[arg(long, short, default_value_t = 0)]
     delay_ms: u64,
+    /// output mode
+    #[arg(short, long, value_enum, default_value_t = OutputMode::File)]
+    mode: OutputMode,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum OutputMode {
+    /// Output will be console
+    Console,
+    /// Output will be file
+    File,
 }
 
 fn main() {
@@ -37,7 +49,15 @@ fn main() {
 
     let characters = gen_all_chars();
     let mut nb_perm = 0;
-    let mut f_password = File::create(args.output_file_name).expect("Unable to create file");
+
+    // Choose between console mode or file mode according to user intput
+    let mut out_writer = match args.mode {
+        OutputMode::Console => Box::new(io::stdout()) as Box<dyn Write>,
+        OutputMode::File => {
+            let f_password = File::create(args.output_file_name).expect("Unable to create file");
+            Box::new(f_password) as Box<dyn Write>
+        }
+    };
     
     for n in args.min_size..args.max_size + 1 {
         _ = characters
@@ -46,11 +66,10 @@ fn main() {
             .permutations(n.try_into().unwrap())
             .for_each(|perm| {
                 nb_perm += 1;
-                _ = write!(f_password, "{}\n", perm.into_iter().collect::<String>());
-                //println!("{}", perm.into_iter().collect::<String>());
+                _ = write!(out_writer, "{}\n", perm.into_iter().collect::<String>());
                 std::thread::sleep(Duration::from_millis(args.delay_ms));
-            });
-    }
+            })
+        }
     let duration = start.elapsed();
 
     println!(
